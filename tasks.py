124027704b589584e72e180ca639c51cb81d0daa
Тимур <zamilotimur@gmail.com>
2026-08-19 11:31:44 +0300

    задачи

--- tasks.py
+++ tasks.py
@@ -2,3 +2,17 @@
 def square(n):
     return n ** 2
 square(6)
+
+#Задача_2
+def add(a, b):
+    return (a + b)
+add(3, 7)
+
+#Задача_3
+def get_evens(*n):
+    a  = []
+    for k in n:
+        if k % 2 == 0:
+            a.append(k)
+    return a
+get_evens(1, 2, 3, 4, 5)
